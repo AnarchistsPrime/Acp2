9512dd529b4e8119ec0a56f5908b8c90ffbd175c
ACP <anarchistsprime@gmail.com>
2016-11-16 22:32:29 +1000

    Update alert.cpp

--- src/alert.cpp
+++ src/alert.cpp
@@ -19,7 +19,7 @@ using namespace std;
 map<uint256, CAlert> mapAlerts;
 CCriticalSection cs_mapAlerts;
 
-static const char* pszMainKey = "04fc9702847840aaf195de8442ebecedf5b095cdbb9bc716bda9110971b28a49e0ead8564ff0db22209e0374782c093bb899692d524e9d6a6956e7c5ecbcd68284";
+static const char* pszMainKey = "5JY4pyqXqkx2g5UmB3MCYkuSSa1ysJMY6ebTCE8wT2kHfohWvWc";
 static const char* pszTestKey = "04302390343f91cc401d56d68b123028bf52e5fca1939df127f63c6467cdf9c8e2c14b61104cf817d0b780da337893ecc4aaff1309e536162dabbdb45200ca2b0a";
 
 void CUnsignedAlert::SetNull()
