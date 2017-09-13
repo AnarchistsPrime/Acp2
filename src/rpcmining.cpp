250039942b219242660cf160fa6b7017670dd949
ACP <anarchistsprime@gmail.com>
2017-09-13 23:20:56 +1000

    Update rpcmining.cpp

--- src/rpcmining.cpp
+++ src/rpcmining.cpp
@@ -72,6 +72,7 @@ Value getmininginfo(const Array& params, bool fHelp)
     obj.push_back(Pair("blocks",        (int)nBestHeight));
     obj.push_back(Pair("currentblocksize",(uint64_t)nLastBlockSize));
     obj.push_back(Pair("currentblocktx",(uint64_t)nLastBlockTx));
+    obj.push_back(Pair("difficulty", (double)GetDifficulty(NULL, miningAlgo)));
     obj.push_back(Pair("difficulty_sha256", (double)GetDifficulty(NULL, ALGO_SHA256)));
     obj.push_back(Pair("difficulty_groestl", (double)GetDifficulty(NULL, ALGO_GROESTL)));
     obj.push_back(Pair("errors",        GetWarnings("statusbar")));
